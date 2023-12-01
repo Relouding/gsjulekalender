fcb0be13b1f4a67bb7bbe6b8a70a30e09b971acf
Andreas <74594568+Relouding@users.noreply.github.com>
2023-12-01 05:27:14 +0100

    eighth commit

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -4,26 +4,35 @@ document.addEventListener("DOMContentLoaded", function () {
         overlay.classList.toggle("hide");
     }
 
-    function containerClickHandler(containerId, overlayId) {
+    function containerClickHandler(containerId, overlay1Id, overlay2Id, overlay3Id) {
         const container = document.getElementById(containerId);
         if (!container.classList.contains("closed")) {
-            toggleOverlay(overlayId);
+            toggleOverlay(overlay1Id);
+            toggleOverlay(overlay2Id);
+            toggleOverlay(overlay3Id);
+        } else {
+            // Add shake animation
+            container.classList.add("shake");
+            // Remove shake animation
+            setTimeout(() => {
+                container.classList.remove("shake");
+            }, 500);
         }
     }
 
     document.getElementById("luke_en").addEventListener("click", function () {
-        containerClickHandler("luke_en", "overlay_en");
+        containerClickHandler("luke_en", "overlay_en_en", "overlay_to_en", "overlay_tre_en");
     });
 
     document.getElementById("luke_to").addEventListener("click", function () {
-        containerClickHandler("luke_to", "overlay_to");
+        containerClickHandler("luke_to", "overlay_en_to", "overlay_to_to", "overlay_tre_to");
     });
 
     document.getElementById("luke_tre").addEventListener("click", function () {
-        containerClickHandler("luke_tre", "overlay_tre");
+        containerClickHandler("luke_tre", "overlay_en_tre", "overlay_to_tre", "overlay_tre_tre");
     });
 
     document.getElementById("luke_fire").addEventListener("click", function () {
-        containerClickHandler("luke_fire", "overlay_fire");
+        containerClickHandler("luke_fire", "overlay_en_fire", "overlay_to_fire", "overlay_tre_fire");
     });
 });
